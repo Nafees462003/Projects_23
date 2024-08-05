267f779480d9e3df3a7f718a859055a084d70f77
Shahrear Nafees <159715522+Nafees462003@users.noreply.github.com>
2024-08-05 07:43:37 +0600

    Add files via upload

---
+++ esp32_sd_gif.ino
@@ -0,0 +1,136 @@
+#include <SD.h>
+#include <SPI.h>
+#include <FS.h>
+#include <SPIFFS.h>
+#include <TFT_eSPI.h>      // Install this library with the Arduino Library Manager
+                           // Don't forget to configure the driver for the display!
+
+#include <AnimatedGIF.h>   // Install this library with the Arduino Library Manager
+
+#define SD_CS_PIN 12 // Chip Select Pin (CS) for SD card Reader
+
+AnimatedGIF gif;
+File gifFile;              // Global File object for the GIF file
+TFT_eSPI tft = TFT_eSPI(); 
+
+const char *filename = "/200w.gif";   // Change to load other gif files in images/GIF
+void setup()
+{
+  Serial.begin(115200);
+  tft.begin();
+  tft.setRotation(3); // Adjust Rotation of your screen (0-3)
+  tft.fillScreen(TFT_BLACK);
+
+  // Initialize SD card
+  Serial.println("SD card initialization...");
+  if (!SD.begin(SD_CS_PIN))
+  {
+    Serial.println("SD card initialization failed!");
+    return;
+  }
+
+  // Initialize SPIFFS
+  Serial.println("Initialize SPIFFS...");
+  if (!SPIFFS.begin(true))
+  {
+    Serial.println("SPIFFS initialization failed!");
+  }
+
+  // Reformmating the SPIFFS to have space if a large GIF is loaded
+  // You could run out of SPIFFS storage space if you load more than one image or a large GIF
+  // You can also increase the SPIFFS storage space by changing the partition of the ESP32
+  //
+  Serial.println("Formatting SPIFFS...");
+  SPIFFS.format(); // This will erase all the files, change as needed, SPIFFs is non-volatile memory
+  Serial.println("SPIFFS formatted successfully.");
+
+  // Open GIF file from SD card
+  Serial.println("Openning GIF file from SD card...");
+  File sdFile = SD.open(filename);
+  if (!sdFile)
+  {
+    Serial.println("Failed to open GIF file from SD card!");
+    return;
+  }
+
+  // Create a file in SPIFFS to store the GIF
+  File spiffsFile = SPIFFS.open(filename, FILE_WRITE, true);
+  if (!spiffsFile)
+  {
+    Serial.println("Failed to copy GIF in SPIFFS!");
+    return;
+  }
+
+  // Read the GIF from SD card and write to SPIFFS
+  Serial.println("Copy GIF in SPIFFS...");
+  byte buffer[512];
+  while (sdFile.available())
+  {
+    int bytesRead = sdFile.read(buffer, sizeof(buffer));
+    spiffsFile.write(buffer, bytesRead);
+  }
+
+  spiffsFile.close();
+  sdFile.close();
+
+  // Initialize the GIF
+  Serial.println("Starting animation...");
+  gif.begin(BIG_ENDIAN_PIXELS);
+}
+
+void loop()
+{
+  if (gif.open(filename, fileOpen, fileClose, fileRead, fileSeek, GIFDraw))
+  {
+    tft.startWrite(); // The TFT chip slect is locked low
+    while (gif.playFrame(true, NULL))
+    {
+    }
+    gif.close();
+    tft.endWrite(); // Release TFT chip select for the SD Card Reader
+  }
+}
+
+// Callbacks for file operations for the Animated GIF Lobrary
+void *fileOpen(const char *filename, int32_t *pFileSize)
+{
+  gifFile = SPIFFS.open(filename, FILE_READ);
+  *pFileSize = gifFile.size();
+  if (!gifFile)
+  {
+    Serial.println("Failed to open GIF file from SPIFFS!");
+  }
+  return &gifFile;
+}
+
+void fileClose(void *pHandle)
+{
+  gifFile.close();
+}
+
+int32_t fileRead(GIFFILE *pFile, uint8_t *pBuf, int32_t iLen)
+{
+  int32_t iBytesRead;
+  iBytesRead = iLen;
+  if ((pFile->iSize - pFile->iPos) < iLen)
+    iBytesRead = pFile->iSize - pFile->iPos;
+  if (iBytesRead <= 0)
+    return 0;
+
+  gifFile.seek(pFile->iPos);
+  int32_t bytesRead = gifFile.read(pBuf, iLen);
+  pFile->iPos += iBytesRead;
+
+  return bytesRead;
+}
+
+int32_t fileSeek(GIFFILE *pFile, int32_t iPosition)
+{
+  if (iPosition < 0)
+    iPosition = 0;
+  else if (iPosition >= pFile->iSize)
+    iPosition = pFile->iSize - 1;
+  pFile->iPos = iPosition;
+  gifFile.seek(pFile->iPos);
+  return iPosition;
+}
